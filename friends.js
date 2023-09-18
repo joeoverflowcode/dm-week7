bc66c8d00de2284111551ea9ba4c2db1dd016003
Joe Aguado <aguado.joe@gmail.com>
2023-09-18 09:35:07 -0500

    setup push -j

---
+++ friends.js
@@ -0,0 +1,5 @@
+//write a function that prints the following string for each user in the users array:
+//"${person}'s friends are ${friends}" where person is the person's name, and friends is a 
+//collection of their friends' names.
+
+
