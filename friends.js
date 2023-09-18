86f203fb7ca75cfebcf86b0c64f3040ab803abae
Joe Aguado <aguado.joe@gmail.com>
2023-09-18 10:03:30 -0500

    answer

--- friends.js
+++ friends.js
@@ -2,4 +2,39 @@
 //"${person}'s friends are ${friends}" where person is the person's name, and friends is a 
 //collection of their friends' names.
 
+let users = {
+    1: {
+        name: "John Smith",
+        friends: [3,5]
+    },
+    2: {
+        name: "Christie Agatha",
+        friends: [3,4,5]
+    },
+    3: {
+        name: "Max Johnson",
+        friends: [1,2,5]
+    },
+    4: {
+        name: "Joan Carlson",
+        friends: [2]
+    },
+    5: {
+        name: "Steve Herman",
+        friends: [2]
+    }
+}
 
+const findFriends = (obj) => {
+    for(let id in obj){
+        let friendNames = []
+        let person = obj[id]
+        let { name, friends } = person
+
+        friends.forEach(friendId => friendNames.push(obj[friendId].name))
+
+        console.log(`${name}'s friends are ${friendNames}`)
+    }
+}
+
+findFriends(users)
